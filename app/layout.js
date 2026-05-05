c0a3fa74376a5e7fd6ad598f26122cb132f1978c
crelgios <creligios@gmail.com>
2026-05-05 08:14:53 +0530

    js update

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,18 @@
 import "./globals.css";
-export const metadata = { title: "Free PDF Tools Online", description: "Convert JPG, PNG and merge PDF files online for free." };
+
+export const metadata = {
+  title: "Free PDF Tools Online",
+  description: "Convert JPG, PNG and merge PDF files online for free.",
+  
+  verification: {
+    google: "Jr5DPyU3bIUowIsWmtUdP3ppLM6qyUwnMjGBgPcgZc0",
+  },
+};
+
 export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}</body></html>;
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
