b9883c23b9ac7c40422928ceb0774ad81c2ea529
crelgios <creligios@gmail.com>
2026-05-05 12:03:02 +0530

    yes

---
+++ public/sitemap.js
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+<url>
+  <loc>https://ali-web-tools.vercel.app/en</loc>
+</url>
+
+<url>
+  <loc>https://ali-web-tools.vercel.app/en/jpg-to-pdf</loc>
+</url>
+
+<url>
+  <loc>https://ali-web-tools.vercel.app/en/png-to-pdf</loc>
+</url>
+
+<url>
+  <loc>https://ali-web-tools.vercel.app/en/merge-pdf</loc>
+</url>
+
+</urlset>
