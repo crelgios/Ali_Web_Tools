d519ba236c1d403ba76650560eb1dedd7b71b5ba
crelgios <creligios@gmail.com>
2026-05-06 23:19:37 +0530

    yes

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import "./globals.css";
 import { createPageMetadata } from "../lib/seo";
 
 export const metadata = createPageMetadata("en", "home");
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
